05a1ba43be5eba0a1aadbac5d2009a1926b7a799
nnodim <nnodimobinna@gmail.com>
2023-09-20 19:34:36 +0100

    innoconnect

--- blog.js
+++ blog.js
@@ -1,21 +1,18 @@
 const backToTop = document.getElementById('back2Top');
- 
 
 // window.addEventListener("scroll", (e) =>{
 //     if (window.scroll >= 400){
 //       backToTop.classList.remove("hidden");
-//       backToTop.classList.add("block")  
+//       backToTop.classList.add("block")
 //     }
 //     else{
 //         backToTop.classList.add("hidden");
-//         backToTop.classList.remove("block")  
+//         backToTop.classList.remove("block")
 //     }
 // })
 
-backToTop.addEventListener('click', (e) =>{
+backToTop.addEventListener('click', (e) => {
   e.preventDefault();
 
-  window.scroll({top:0, left:0, behavior:"smooth"});
-
-} );
-
+  window.scroll({ top: 0, left: 0, behavior: 'smooth' });
+});
